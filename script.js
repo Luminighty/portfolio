461c058b0fc0eda9d36fcd902195a6965c7dc085
Luminighty <balazs922@gmail.hu>
2019-07-09 14:48:33 +0200

    Added CV

--- script.js
+++ script.js
@@ -4,8 +4,10 @@ function $$(query) { return document.querySelectorAll(query); }
 let activeContainer = $(".container.active");
 
 $(".menu").addEventListener("click", (e) => {
-	activeContainer.classList.remove("active");
-	activeContainer = $(`#${e.target.dataset.to}`);
-	console.log(e.target.dataset.to);
-	activeContainer.classList.add("active");
+	if(e.target.dataset.to !== undefined) {
+		activeContainer.classList.remove("active");
+		activeContainer = $(`#${e.target.dataset.to}`);
+		console.log(e.target.dataset.to);
+		activeContainer.classList.add("active");
+	}
 });
